bc8c4736bfa4c5e69d79ccedb6049b358484b7f7
Erfan Nariman <erfannariman@gmail.com>
2020-09-27 23:11:10 +0200

    changes for version 0.0.21

--- setup.py
+++ setup.py
@@ -7,19 +7,20 @@ requirements = ["pynput>=1.7.1"]
 
 setup(
     name="keyboard",
-    version="0.0.2",
+    version="0.0.21",
     author="Erfan Nariman",
     author_email="erfannariman@gmail.com",
     description="Module to do random keystroaks or mouse movements "
                 "to prevent your laptop going to sleep",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    keywords="keyboard, mouse, sleep",
     url="https://github.com/erfannariman/keyboard",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
         "console_scripts": [
-            "keyboard=src.keyboard:run"
+            "keyboard=keyboard.keyboard:do_keyboard"
         ]
     },
     classifiers=[
@@ -28,4 +29,8 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    project_urls={
+        "Bug Reports": "https://github.com/erfannariman/keyboard/issues",
+        "Source": "https://github.com/erfannariman/keyboard"
+    }
 )
